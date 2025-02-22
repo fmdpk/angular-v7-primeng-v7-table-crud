4b1074d6c3cc6d57a8b9572bda6ae8043b1112e4
farzin mossaed <fmossaed@gmail.com>
2025-02-22 09:05:20 +0330

    add dialog to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-export interface Car { vin: string; brand: string; year: number; color: string; isNew: boolean }
+export interface Car {
+  vin: string;
+  brand: string;
+  year: number;
+  color: string;
+  isNew: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   cars: Car[];
-  countOfYear: number = 0
-  oldValue: number = 0
-  newValue: number = 0
+  countOfYear: number = 0;
+  oldValue: number = 0;
+  newValue: number = 0;
+  display: boolean = false;
 
-  brands: { label: string, value: string }[] = [
-    { label: 'Select Item', value: 'Select Item' },
-    { label: 'Audi', value: 'Audi' },
-    { label: 'BMW', value: 'BMW' },
-    { label: 'Fiat', value: 'Fiat' },
-    { label: 'Ford', value: 'Ford' },
-    { label: 'Honda', value: 'Honda' },
-    { label: 'Jaguar', value: 'Jaguar' },
-    { label: 'Mercedes', value: 'Mercedes' },
-    { label: 'Renault', value: 'Renault' },
-    { label: 'VW', value: 'VW' },
-    { label: 'Volvo', value: 'Volvo' }
+  brands: { label: string; value: string }[] = [
+    { label: "Select Item", value: "Select Item" },
+    { label: "Audi", value: "Audi" },
+    { label: "BMW", value: "BMW" },
+    { label: "Fiat", value: "Fiat" },
+    { label: "Ford", value: "Ford" },
+    { label: "Honda", value: "Honda" },
+    { label: "Jaguar", value: "Jaguar" },
+    { label: "Mercedes", value: "Mercedes" },
+    { label: "Renault", value: "Renault" },
+    { label: "VW", value: "VW" },
+    { label: "Volvo", value: "Volvo" },
   ];
 
   rowGroupMetadata: any;
 
-
   constructor() {
     this.cars = [
-      { vin: 'a1', brand: 'VW', year: 1998, color: 'White', isNew: false },
-      { vin: 'a2', brand: 'Audi', year: 1987, color: 'Black', isNew: false }
+      { vin: "a1", brand: "VW", year: 1998, color: "White", isNew: false },
+      { vin: "a2", brand: "Audi", year: 1987, color: "Black", isNew: false },
     ];
 
     this.cars.forEach((item: Car) => {
-      this.countOfYear += item.year
-    })
+      this.countOfYear += item.year;
+    });
 
     this.updateRowGroupMetaData();
   }
@@ -51,14 +57,11 @@ export class AppComponent {
         let brand = rowData.brand;
         if (i == 0) {
           this.rowGroupMetadata[brand] = { index: 0, size: 1 };
-        }
-        else {
+        } else {
           let previousRowData = this.cars[i - 1];
           let previousRowGroup = previousRowData.brand;
-          if (brand === previousRowGroup)
-            this.rowGroupMetadata[brand].size++;
-          else
-            this.rowGroupMetadata[brand] = { index: i, size: 1 };
+          if (brand === previousRowGroup) this.rowGroupMetadata[brand].size++;
+          else this.rowGroupMetadata[brand] = { index: i, size: 1 };
         }
       }
     }
@@ -66,44 +69,58 @@ export class AppComponent {
 
   addNewRow() {
     this.cars.unshift({
-      vin: 'a' + (+this.cars.length + 1), brand: 'Select Item', year: 0, color: '', isNew: true
-    })
+      vin: "a" + (+this.cars.length + 1),
+      brand: "Select Item",
+      year: 0,
+      color: "",
+      isNew: true,
+    });
   }
 
   submit() {
-    let newItems = this.cars.filter(item => {
-      return item.isNew === true
-    })
+    let newItems = this.cars.filter((item) => {
+      return item.isNew === true;
+    });
 
     console.log(newItems);
     console.log(this.cars);
 
-    this.cars.forEach(item => {
-      item.isNew = false
-    })
+    this.cars.forEach((item) => {
+      item.isNew = false;
+    });
 
-    var alertMessage = newItems.map((obj) => {
-      return "Vin: " + obj.vin + ", Color: " + obj.color + ', Brand: ' + obj.brand + ', year: ' + obj.year
-    }).join("\n");
+    var alertMessage = newItems
+      .map((obj) => {
+        return (
+          "Vin: " +
+          obj.vin +
+          ", Color: " +
+          obj.color +
+          ", Brand: " +
+          obj.brand +
+          ", year: " +
+          obj.year
+        );
+      })
+      .join("\n");
 
     if (newItems.length) {
       alert(alertMessage);
     } else {
-      alert('موردی برای نمایش وجود ندارد')
+      alert("موردی برای نمایش وجود ندارد");
     }
-
   }
 
   onFocusYear(event: FocusEvent, rowData: Car) {
     console.log(event);
-    this.oldValue = +rowData.year
+    this.oldValue = +rowData.year;
   }
 
   onFocusoutYear(event: FocusEvent, rowData: Car) {
     console.log(event);
-    this.newValue = +rowData.year
-    let difference = this.newValue - this.oldValue
-    this.countOfYear += difference
+    this.newValue = +rowData.year;
+    let difference = this.newValue - this.oldValue;
+    this.countOfYear += difference;
   }
 
   onKeydownYear(event: KeyboardEvent, rowData: Car) {
@@ -117,14 +134,18 @@ export class AppComponent {
   }
 
   removeRow(rowData: Car) {
-    this.cars = this.cars.filter(item => {
-      return item.vin !== rowData.vin
-    })
+    this.cars = this.cars.filter((item) => {
+      return item.vin !== rowData.vin;
+    });
 
-    this.countOfYear -= +rowData.year
+    this.countOfYear -= +rowData.year;
   }
 
   onChangeYear(year: number) {
     // this.countOfYear += +year
+  }
+
+  showDialog() {
+    this.display = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
-import { DropdownModule } from 'primeng/dropdown';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { FormsModule } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TableModule } from "primeng/table";
+import { DropdownModule } from "primeng/dropdown";
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { DropdownComponent } from "./components/dropdown/dropdown.component";
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DropdownComponent
-  ],
+  declarations: [AppComponent, DropdownComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -25,8 +23,9 @@ import { ButtonModule } from 'primeng/button';
     TableModule,
     DropdownModule,
     ScrollingModule,
+    DialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
